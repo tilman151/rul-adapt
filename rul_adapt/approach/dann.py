be6d433cfbc0f9af08fd4c8cab289c78d66aa951
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-12-01 16:19:20 +0100

    feat: add dann approach

---
+++ rul_adapt/approach/dann.py
@@ -0,0 +1,145 @@
+from typing import Any, Optional, Dict
+
+import torch
+import torchmetrics
+from torch import nn
+from torch.optim import SGD
+from torch.optim.lr_scheduler import StepLR
+
+import rul_adapt.loss
+from rul_adapt.approach.abstract import AdaptionApproach
+
+
+class DannApproach(AdaptionApproach):
+    dann_loss: rul_adapt.loss.DomainAdversarialLoss
+
+    def __init__(
+        self,
+        dann_factor: float,
+        lr: float,
+        weight_decay: float = 0.0,
+        lr_decay_factor: Optional[float] = None,
+        lr_decay_epochs: Optional[int] = None,
+        use_rmse: bool = False,
+    ):
+        super().__init__()
+
+        if not ((lr_decay_factor is None) == (lr_decay_epochs is None)):
+            raise ValueError(
+                "Either both lr_decay_factor and lr_decay_epoch "
+                "need to be specified or neither of them."
+            )
+
+        self.dann_factor = dann_factor
+        self.weight_decay = weight_decay
+        self.lr = lr
+        self.lr_decay_factor = lr_decay_factor
+        self.lr_decay_epochs = lr_decay_epochs
+        self.use_rmse = use_rmse
+
+        # training metrics
+        self.train_mse = torchmetrics.MeanSquaredError(squared=not self.use_rmse)
+
+        # validation metrics
+        self.val_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_source_score = rul_adapt.loss.RULScore()
+        self.val_target_score = rul_adapt.loss.RULScore()
+
+        # testing metrics
+        self.test_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_source_score = rul_adapt.loss.RULScore()
+        self.test_target_score = rul_adapt.loss.RULScore()
+
+        self.save_hyperparameters()
+
+    def set_model(
+        self,
+        feature_extractor: nn.Module,
+        regressor: nn.Module,
+        domain_disc: Optional[nn.Module] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        if domain_disc is None:
+            raise ValueError(
+                "No domain discriminator was set. This approach is unlikely to work."
+            )
+
+        super().set_model(feature_extractor, regressor, *args, **kwargs)
+        self.dann_loss = rul_adapt.loss.DomainAdversarialLoss(domain_disc)
+
+    @property
+    def domain_disc(self):
+        """The domain discriminator network."""
+        if hasattr(self, "dann_loss"):
+            return self.dann_loss.domain_disc
+        else:
+            raise RuntimeError("Domain disc used before 'set_model' was called.")
+
+    def configure_optimizers(self) -> Dict[str, Any]:
+        optim = SGD(self.parameters(), self.lr, weight_decay=self.weight_decay)
+        result: Dict[str, Any] = {"optimizer": optim}
+
+        if (self.lr_decay_factor is not None) and (self.lr_decay_epochs is not None):
+            scheduler = StepLR(optim, self.lr_decay_epochs, self.lr_decay_factor)
+            result["lr_scheduler"] = {"scheduler": scheduler}
+
+        return result
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        return self.regressor(self.feature_extractor(inputs))
+
+    def training_step(
+        self, source: torch.Tensor, source_labels: torch.Tensor, target: torch.Tensor
+    ) -> torch.Tensor:
+        source = self.feature_extractor(source)
+        target = self.feature_extractor(target)
+
+        rul_preds = self.regressor(source)
+        mse_loss = self.train_mse(rul_preds, source_labels)
+
+        dann_loss = self.dann_loss(source, target)
+
+        loss = mse_loss + self.dann_factor * dann_loss
+
+        self.log("train_loss", loss)
+        self.log("train_source_mse", self.train_mse)
+        self.log("train_dann", self.dann_loss)
+
+        return loss
+
+    def validation_step(
+        self, features: torch.Tensor, labels: torch.Tensor, dataloader_idx: int
+    ) -> None:
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.val_source_rmse(predictions, labels)
+            self.val_source_score(predictions, labels)
+            self.log("val_source_rmse", self.val_source_rmse)
+            self.log("val_source_score", self.val_source_score)
+        elif dataloader_idx == 1:
+            self.val_target_rmse(predictions, labels)
+            self.val_target_score(predictions, labels)
+            self.log("val_target_rmse", self.val_target_rmse)
+            self.log("val_target_score", self.val_target_score)
+        else:
+            raise RuntimeError(f"Unexpected val data loader idx {dataloader_idx}")
+
+    def test_step(
+        self, features: torch.Tensor, labels: torch.Tensor, dataloader_idx: int
+    ) -> None:
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.test_source_rmse(predictions, labels)
+            self.test_source_score(predictions, labels)
+            self.log("test_source_rmse", self.test_source_rmse)
+            self.log("test_source_score", self.test_source_score)
+        elif dataloader_idx == 1:
+            self.test_target_rmse(predictions, labels)
+            self.test_target_score(predictions, labels)
+            self.log("test_target_rmse", self.test_target_rmse)
+            self.log("test_target_score", self.test_target_score)
+        else:
+            raise RuntimeError(f"Unexpected test data loader idx {dataloader_idx}")
---
+++ tests/test_approach/test_dann.py
@@ -0,0 +1,258 @@
+from unittest import mock
+
+import pytest
+import torch
+from torch import nn
+from torchmetrics import Metric
+
+import rul_adapt.loss
+from rul_adapt import model
+from rul_adapt.approach.dann import DannApproach
+
+
+@pytest.fixture()
+def models():
+    fe = model.LstmExtractor(14, [16], 4)
+    reg = model.FullyConnectedHead(4, [1], act_func_on_last_layer=False)
+    disc = model.FullyConnectedHead(4, [1], act_func_on_last_layer=False)
+
+    return fe, reg, disc
+
+
+@pytest.fixture()
+def approach(models):
+    feature_extractor, regressor, domain_disc = models
+    approach = DannApproach(1.0, 0.001)
+    approach.set_model(feature_extractor, regressor, domain_disc)
+
+    return approach
+
+
+@pytest.fixture()
+def mocked_approach():
+    feature_extractor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 8))
+    regressor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
+    domain_disc = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
+    approach = DannApproach(1.0, 0.001)
+    approach.set_model(feature_extractor, regressor, domain_disc)
+
+    return approach
+
+
+@pytest.fixture()
+def inputs():
+    return (
+        torch.randn(10, 14, 30),  # source
+        torch.arange(10, dtype=torch.float)[:, None],  # source_labels
+        torch.randn(10, 14, 30),  # target
+    )
+
+
+def test_set_model(models):
+    feature_extractor, regressor, domain_disc = models
+    approach = DannApproach(1.0, 0.001)
+    approach.set_model(feature_extractor, regressor, domain_disc)
+
+    assert approach.feature_extractor is feature_extractor
+    assert approach.regressor is regressor
+    assert hasattr(approach, "dann_loss")  # dann loss was created
+    assert approach.dann_loss.domain_disc is domain_disc  # disc was assigned correctly
+    assert approach.domain_disc is domain_disc  # domain_disc property works
+
+
+@pytest.mark.parametrize("weight_decay", [0.0, 0.1])
+def test_configure_optimizer(models, weight_decay):
+    approach = DannApproach(1.0, 0.001, weight_decay)
+    approach.set_model(*models)
+
+    optim_conf = approach.configure_optimizers()
+
+    assert isinstance(optim_conf, dict)
+    assert isinstance(optim_conf["optimizer"], torch.optim.SGD)
+    assert optim_conf["optimizer"].defaults["weight_decay"] == weight_decay
+    assert "lr_scheduler" not in optim_conf
+
+
+@pytest.mark.parametrize("use_rmse", [True, False])
+def test_use_rmse(use_rmse):
+    approach = DannApproach(1.0, 0.001, use_rmse=use_rmse)
+
+    assert approach.use_rmse == use_rmse
+    assert not approach.train_mse.squared == use_rmse  # training uses rmse or mse
+    assert not approach.val_source_rmse.squared  # val and test always uses rmse
+    assert not approach.val_target_rmse.squared
+    assert not approach.test_source_rmse.squared
+    assert not approach.test_target_rmse.squared
+
+
+def test_configure_optimizer_lr_decay(models):
+    lr_decay_factor = 0.1
+    lr_decay_epochs = 100
+    approach = DannApproach(1.0, 0.001, 0.1, lr_decay_factor, lr_decay_epochs)
+    approach.set_model(*models)
+
+    optim_conf = approach.configure_optimizers()
+
+    assert isinstance(optim_conf, dict)
+    assert "lr_scheduler" in optim_conf
+    assert isinstance(optim_conf["lr_scheduler"], dict)
+    assert isinstance(
+        optim_conf["lr_scheduler"]["scheduler"], torch.optim.lr_scheduler.StepLR
+    )
+    assert optim_conf["lr_scheduler"]["scheduler"].gamma == lr_decay_factor
+    assert optim_conf["lr_scheduler"]["scheduler"].step_size == lr_decay_epochs
+
+
+@pytest.mark.parametrize(
+    ["lr_decay_factor", "lr_decay_epochs"], [(None, 100), (0.1, None)]
+)
+def test_lr_decay_validation(lr_decay_factor, lr_decay_epochs):
+    with pytest.raises(ValueError):
+        DannApproach(1.0, 0.001, 0.1, lr_decay_factor, lr_decay_epochs)
+
+
+@torch.no_grad()
+def test_forward(approach, inputs):
+    features, _, _ = inputs
+
+    outputs = approach(features)
+
+    assert outputs.shape == torch.Size([10, 1])
+
+
+@torch.no_grad()
+def test_train_step(approach, inputs):
+
+    outputs = approach.training_step(*inputs)
+
+    assert outputs.shape == torch.Size([])
+
+
+def test_train_step_backward(approach, inputs):
+
+    outputs = approach.training_step(*inputs)
+    outputs.backward()
+
+    extractor_parameter = next(approach.feature_extractor.parameters())
+    assert extractor_parameter.grad is not None
+
+
+@torch.no_grad()
+def test_val_step(approach, inputs):
+    features, labels, _ = inputs
+
+    approach.validation_step(features, labels, dataloader_idx=0)
+    approach.validation_step(features, labels, dataloader_idx=1)
+    with pytest.raises(RuntimeError):
+        approach.validation_step(features, labels, dataloader_idx=2)
+
+
+@torch.no_grad()
+def test_test_step(approach, inputs):
+    features, labels, _ = inputs
+
+    approach.test_step(features, labels, dataloader_idx=0)
+    approach.test_step(features, labels, dataloader_idx=1)
+    with pytest.raises(RuntimeError):
+        approach.test_step(features, labels, dataloader_idx=2)
+
+
+@torch.no_grad()
+def test_train_step_logging(mocked_approach, inputs):
+    approach = mocked_approach
+    approach.train_mse = mock.MagicMock(Metric)
+    approach.dann_loss = mock.MagicMock(rul_adapt.loss.DomainAdversarialLoss)
+    approach.log = mock.MagicMock()
+
+    approach.training_step(*inputs)
+
+    approach.train_mse.assert_called_once()
+    approach.dann_loss.assert_called_once()
+
+    approach.log.assert_has_calls(
+        [
+            mock.call("train_loss", mock.ANY),
+            mock.call("train_source_mse", approach.train_mse),
+            mock.call("train_dann", approach.dann_loss),
+        ]
+    )
+
+
+@torch.no_grad()
+def test_val_step_logging(mocked_approach, inputs):
+    approach = mocked_approach
+    features, labels, _ = inputs
+    approach.val_source_rmse = mock.MagicMock(Metric)
+    approach.val_source_score = mock.MagicMock(Metric)
+    approach.val_target_rmse = mock.MagicMock(Metric)
+    approach.val_target_score = mock.MagicMock(Metric)
+    approach.log = mock.MagicMock()
+
+    # check source data loader call
+    approach.validation_step(features, labels, dataloader_idx=0)
+    approach.val_source_rmse.assert_called_once()
+    approach.val_source_score.assert_called_once()
+    approach.val_target_rmse.assert_not_called()
+    approach.val_target_score.assert_not_called()
+    approach.log.assert_has_calls(
+        [
+            mock.call("val_source_rmse", approach.val_source_rmse),
+            mock.call("val_source_score", approach.val_source_score),
+        ]
+    )
+
+    approach.val_source_rmse.reset_mock()
+    approach.val_source_score.reset_mock()
+
+    # check target data loader call
+    approach.validation_step(features, labels, dataloader_idx=1)
+    approach.val_source_rmse.assert_not_called()
+    approach.val_source_score.assert_not_called()
+    approach.val_target_rmse.assert_called_once()
+    approach.val_target_score.assert_called_once()
+    approach.log.assert_has_calls(
+        [
+            mock.call("val_target_rmse", approach.val_target_rmse),
+            mock.call("val_target_score", approach.val_target_score),
+        ]
+    )
+
+
+@torch.no_grad()
+def test_test_step_logging(mocked_approach, inputs):
+    approach = mocked_approach
+    features, labels, _ = inputs
+    approach.test_source_rmse = mock.MagicMock(Metric)
+    approach.test_source_score = mock.MagicMock(Metric)
+    approach.test_target_rmse = mock.MagicMock(Metric)
+    approach.test_target_score = mock.MagicMock(Metric)
+    approach.log = mock.MagicMock()
+
+    # check source data loader call
+    approach.test_step(features, labels, dataloader_idx=0)
+    approach.test_source_rmse.assert_called_once()
+    approach.test_source_score.assert_called_once()
+    approach.test_target_rmse.assert_not_called()
+    approach.test_target_score.assert_not_called()
+    approach.log.assert_has_calls(
+        [
+            mock.call("test_source_rmse", approach.test_source_rmse),
+            mock.call("test_source_score", approach.test_source_score),
+        ]
+    )
+
+    approach.test_source_rmse.reset_mock()
+    approach.test_source_score.reset_mock()
+
+    # check target data loader call
+    approach.test_step(features, labels, dataloader_idx=1)
+    approach.test_source_rmse.assert_not_called()
+    approach.test_source_score.assert_not_called()
+    approach.test_target_rmse.assert_called_once()
+    approach.test_target_score.assert_called_once()
+    approach.log.assert_has_calls(
+        [
+            mock.call("test_target_rmse", approach.test_target_rmse),
+            mock.call("test_target_score", approach.test_target_score),
+        ]
+    )
