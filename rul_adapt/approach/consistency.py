7471ea3608b9e348e1e75ad313e39ff2ee73e653
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-01-12 14:17:22 +0100

    feat: add consistency approach

--- rul_adapt/approach/consistency.py
+++ rul_adapt/approach/consistency.py
@@ -1,9 +1,15 @@
-from typing import Tuple
+import copy
+import math
+from itertools import chain
+from typing import Tuple, Optional, Any, List
 
 import torch
 import torchmetrics
+from torch import nn
 
+import rul_adapt.loss
 from rul_adapt.approach.abstract import AdaptionApproach
+from rul_adapt.model import FullyConnectedHead
 
 
 class ConsistencyApproachPretraining(AdaptionApproach):
@@ -15,15 +21,15 @@ class ConsistencyApproachPretraining(AdaptionApproach):
         self.train_loss = torchmetrics.MeanSquaredError(squared=False)
         self.val_loss = torchmetrics.MeanSquaredError(squared=False)
 
+        self.save_hyperparameters()
+
     def configure_optimizers(self) -> torch.optim.SGD:
         return torch.optim.SGD(self.parameters(), lr=self.lr)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         return self.regressor(self.feature_extractor(inputs))
 
-    def training_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
-    ) -> torch.Tensor:
+    def training_step(self, batch: List[torch.Tensor], batch_idx: int) -> torch.Tensor:
         inputs, labels = batch
         predictions = self.forward(inputs)
         loss = self.train_loss(predictions, labels[:, None])
@@ -31,9 +37,7 @@ class ConsistencyApproachPretraining(AdaptionApproach):
 
         return loss
 
-    def validation_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
-    ) -> None:
+    def validation_step(self, batch: List[torch.Tensor], batch_idx: int) -> None:
         inputs, labels = batch
         predictions = self.forward(inputs)
         self.val_loss(predictions, labels[:, None])
@@ -41,4 +45,191 @@ class ConsistencyApproachPretraining(AdaptionApproach):
 
 
 class ConsistencyApproach(AdaptionApproach):
-    pass
+    dann_loss: rul_adapt.loss.DomainAdversarialLoss
+    frozen_feature_extractor: nn.Module
+
+    def __init__(self, consistency_factor: float, lr: float, max_epochs: int):
+        super().__init__()
+
+        self.consistency_factor = consistency_factor
+        self.lr = lr
+        self.max_epochs = max_epochs
+
+        # training metrics
+        self.train_source_loss = torchmetrics.MeanSquaredError(squared=False)
+        self.consistency_loss = rul_adapt.loss.ConsistencyLoss()
+
+        # validation metrics
+        self.val_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_source_score = rul_adapt.loss.RULScore()
+        self.val_target_score = rul_adapt.loss.RULScore()
+
+        # testing metrics
+        self.test_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_source_score = rul_adapt.loss.RULScore()
+        self.test_target_score = rul_adapt.loss.RULScore()
+
+        self.save_hyperparameters()
+
+    def set_model(
+        self,
+        feature_extractor: nn.Module,
+        regressor: nn.Module,
+        domain_disc: Optional[nn.Module] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Set the feature extractor, regressor and domain discriminator for this approach.
+        The discriminator is not allowed to have an activation function on its last
+        layer and needs to use only a single output neuron. It is wrapped by a
+        [DomainAdversarialLoss][rul_adapt.loss.DomainAdversarialLoss].
+        TODO: Add explanation of frozen FE
+        Args:
+            feature_extractor: The feature extraction network.
+            regressor: The RUL regression network.
+            domain_disc: The domain discriminator network.
+        """
+        domain_disc = self._check_domain_disc(domain_disc)
+        super().set_model(feature_extractor, regressor, *args, **kwargs)
+        self.dann_loss = rul_adapt.loss.DomainAdversarialLoss(domain_disc)
+        self.frozen_feature_extractor = copy.deepcopy(feature_extractor)
+        self.frozen_feature_extractor.requires_grad_(False)  # freeze network
+
+    def _check_domain_disc(self, domain_disc: Optional[nn.Module]) -> nn.Module:
+        if domain_disc is None:
+            raise ValueError(
+                "No domain discriminator was set. This approach is unlikely to work."
+            )
+        if (
+            isinstance(domain_disc, FullyConnectedHead)
+            and domain_disc.act_func_on_last_layer
+        ):
+            raise ValueError(
+                "Domain discriminator has an activation function on its last layer. "
+                "This is not allowed due to torch.nn.BCEWithLogitsLoss being used as "
+                "its loss. Please set 'act_func_on_last_layer' to False."
+            )
+
+        return domain_disc
+
+    @property
+    def domain_disc(self):
+        """The domain discriminator network."""
+        if hasattr(self, "dann_loss"):
+            return self.dann_loss.domain_disc
+        else:
+            raise RuntimeError("Domain disc used before 'set_model' was called.")
+
+    @property
+    def dann_factor(self):
+        return 2 / (1 + math.exp(-10 * self.current_epoch / self.max_epochs)) - 1
+
+    def configure_optimizers(self) -> torch.optim.SGD:
+        parameters = chain(
+            self.feature_extractor.parameters(),
+            self.regressor.parameters(),
+            self.dann_loss.parameters(),
+        )  # excludes frozen_feature_extractor from optimization
+        optim = torch.optim.SGD(parameters, lr=self.lr)
+
+        return optim
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        return self.regressor(self.feature_extractor(inputs))
+
+    def training_step(self, batch: List[torch.Tensor], batch_idx: int) -> torch.Tensor:
+        source, source_labels, target = batch
+        source_labels = source_labels[:, None]
+
+        frozen_source = self.frozen_feature_extractor(source)
+        source = self.feature_extractor(source)
+        target = self.feature_extractor(target)
+
+        rul_preds = self.regressor(source)
+        rmse_loss = self.train_source_loss(rul_preds, source_labels)
+
+        dann_loss = self.dann_loss(source, target)
+        consistency_loss = self.consistency_loss(frozen_source, source)
+
+        loss = (
+            rmse_loss
+            + self.dann_factor * dann_loss
+            + self.consistency_factor * consistency_loss
+        )
+
+        self.log("train/loss", loss)
+        self.log("train/source_loss", self.train_source_loss)
+        self.log("train/dann", self.dann_loss)
+        self.log("train/consistency", self.consistency_loss)
+
+        return loss
+
+    def on_train_epoch_start(self) -> None:
+        self.log("train/dann_factor", self.dann_factor)
+
+    def validation_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int
+    ) -> None:
+        """
+        Execute one validation step.
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `val`.
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        features, labels = batch
+        labels = labels[:, None]
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.val_source_rmse(predictions, labels)
+            self.val_source_score(predictions, labels)
+            self.log("val/source_rmse", self.val_source_rmse)
+            self.log("val/source_score", self.val_source_score)
+        elif dataloader_idx == 1:
+            self.val_target_rmse(predictions, labels)
+            self.val_target_score(predictions, labels)
+            self.log("val/target_rmse", self.val_target_rmse)
+            self.log("val/target_score", self.val_target_score)
+        else:
+            raise RuntimeError(f"Unexpected val data loader idx {dataloader_idx}")
+
+    def test_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int
+    ) -> None:
+        """
+        Execute one test step.
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `test`.
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        features, labels = batch
+        labels = labels[:, None]
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.test_source_rmse(predictions, labels)
+            self.test_source_score(predictions, labels)
+            self.log("test/source_rmse", self.test_source_rmse)
+            self.log("test/source_score", self.test_source_score)
+        elif dataloader_idx == 1:
+            self.test_target_rmse(predictions, labels)
+            self.test_target_score(predictions, labels)
+            self.log("test/target_rmse", self.test_target_rmse)
+            self.log("test/target_score", self.test_target_score)
+        else:
+            raise RuntimeError(f"Unexpected test data loader idx {dataloader_idx}")
--- tests/test_approach/test_consistency.py
+++ tests/test_approach/test_consistency.py
@@ -8,12 +8,17 @@ from torch import nn
 from torchmetrics import Metric
 
 from rul_adapt import model
-from rul_adapt.approach import ConsistencyApproachPretraining
+from rul_adapt.approach import ConsistencyApproachPretraining, ConsistencyApproach
 
 
 @pytest.fixture()
 def pretraining_inputs():
     return torch.randn(10, 14, 20), torch.arange(10, dtype=torch.float)
+
+
+@pytest.fixture()
+def inputs(pretraining_inputs):
+    return *pretraining_inputs, torch.randn(10, 14, 20)
 
 
 @pytest.fixture()
@@ -25,9 +30,24 @@ def pretraining_models():
 
 
 @pytest.fixture()
+def models(pretraining_models):
+    disc = model.FullyConnectedHead(20, [1], act_func_on_last_layer=False)
+
+    return *pretraining_models, disc
+
+
+@pytest.fixture()
 def pretraining_approach(pretraining_models):
     approach = ConsistencyApproachPretraining(lr=0.001)
     approach.set_model(*pretraining_models)
+
+    return approach
+
+
+@pytest.fixture()
+def approach(models):
+    approach = ConsistencyApproach(0.1, 0.001, 3000)
+    approach.set_model(*models)
 
     return approach
 
@@ -38,6 +58,17 @@ def mocked_pretraining_approach():
     regressor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
     approach = ConsistencyApproachPretraining(0.001)
     approach.set_model(feature_extractor, regressor)
+
+    return approach
+
+
+@pytest.fixture()
+def mocked_approach():
+    feature_extractor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 20))
+    regressor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
+    disc = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
+    approach = ConsistencyApproach(0.1, 0.001, 3000)
+    approach.set_model(feature_extractor, regressor, disc)
 
     return approach
 
@@ -87,24 +118,179 @@ class TestConsistencyPretraining:
         approach.val_loss.assert_called_once()
         approach.log.assert_called_with("val/loss", approach.val_loss)
 
-    @pytest.mark.integration
-    def test_on_dummy(self):
-        pl.seed_everything(42)
 
-        fd1 = rul_datasets.reader.DummyReader(fd=1)
-        dm = rul_datasets.RulDataModule(fd1, 16)
+class TestConsistencyApproach:
+    def test_forward(self, inputs, approach):
+        features, *_ = inputs
 
-        feature_extractor = model.CnnExtractor(1, [16], 10, fc_units=10)
-        regressor = model.FullyConnectedHead(10, [1], act_func_on_last_layer=False)
+        outputs = approach(features)
 
-        approach = ConsistencyApproachPretraining(0.001)
-        approach.set_model(feature_extractor, regressor)
+        assert outputs.shape == torch.Size([10, 1])
 
-        trainer = pl.Trainer(
-            logger=False,
-            enable_progress_bar=False,
-            enable_model_summary=False,
-            enable_checkpointing=False,
-            max_epochs=10,
+    def test_train_step(self, inputs, approach):
+        outputs = approach.training_step(inputs, batch_idx=0)
+
+        assert outputs.shape == torch.Size([])
+
+    def test_train_step_backward(self, inputs, approach):
+        outputs = approach.training_step(inputs, batch_idx=0)
+        outputs.backward()
+
+        extractor_parameter = next(approach.feature_extractor.parameters())
+        assert extractor_parameter.grad is not None
+
+        frozen_extractor_param = next(approach.frozen_feature_extractor.parameters())
+        assert frozen_extractor_param.grad is None
+
+    @torch.no_grad()
+    def test_val_step(self, inputs, approach):
+        features, labels, _ = inputs
+
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=0)
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=1)
+        with pytest.raises(RuntimeError):
+            approach.validation_step([features, labels], batch_idx=0, dataloader_idx=2)
+
+    @torch.no_grad()
+    def test_train_step_logging(self, inputs, mocked_approach):
+        approach = mocked_approach
+        approach.train_source_loss = mock.MagicMock(Metric)
+        approach.dann_loss = mock.MagicMock(Metric)
+        approach.consistency_loss = mock.MagicMock(Metric)
+        approach.log = mock.MagicMock()
+
+        approach.training_step(inputs, batch_idx=0)
+
+        approach.train_source_loss.assert_called_once()
+        approach.dann_loss.assert_called_once()
+        approach.consistency_loss.assert_called_once()
+        approach.log.assert_has_calls(
+            [
+                mock.call("train/loss", mock.ANY),
+                mock.call("train/source_loss", approach.train_source_loss),
+                mock.call("train/dann", approach.dann_loss),
+                mock.call("train/consistency", approach.consistency_loss),
+            ]
         )
-        trainer.fit(approach, dm)
+
+    @torch.no_grad()
+    def test_val_step_logging(self, mocked_approach, inputs):
+        approach = mocked_approach
+
+        features, labels, _ = inputs
+        approach.val_source_rmse = mock.MagicMock(Metric)
+        approach.val_source_score = mock.MagicMock(Metric)
+        approach.val_target_rmse = mock.MagicMock(Metric)
+        approach.val_target_score = mock.MagicMock(Metric)
+        approach.log = mock.MagicMock()
+
+        # check source data loader call
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=0)
+        approach.val_source_rmse.assert_called_once()
+        approach.val_source_score.assert_called_once()
+        approach.val_target_rmse.assert_not_called()
+        approach.val_target_score.assert_not_called()
+        approach.log.assert_has_calls(
+            [
+                mock.call("val/source_rmse", approach.val_source_rmse),
+                mock.call("val/source_score", approach.val_source_score),
+            ]
+        )
+
+        approach.val_source_rmse.reset_mock()
+        approach.val_source_score.reset_mock()
+
+        # check target data loader call
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=1)
+        approach.val_source_rmse.assert_not_called()
+        approach.val_source_score.assert_not_called()
+        approach.val_target_rmse.assert_called_once()
+        approach.val_target_score.assert_called_once()
+        approach.log.assert_has_calls(
+            [
+                mock.call("val/target_rmse", approach.val_target_rmse),
+                mock.call("val/target_score", approach.val_target_score),
+            ]
+        )
+
+    @torch.no_grad()
+    def test_test_step_logging(self, mocked_approach, inputs):
+        approach = mocked_approach
+        features, labels, _ = inputs
+        approach.test_source_rmse = mock.MagicMock(Metric)
+        approach.test_source_score = mock.MagicMock(Metric)
+        approach.test_target_rmse = mock.MagicMock(Metric)
+        approach.test_target_score = mock.MagicMock(Metric)
+        approach.log = mock.MagicMock()
+
+        # check source data loader call
+        approach.test_step([features, labels], batch_idx=0, dataloader_idx=0)
+        approach.test_source_rmse.assert_called_once()
+        approach.test_source_score.assert_called_once()
+        approach.test_target_rmse.assert_not_called()
+        approach.test_target_score.assert_not_called()
+        approach.log.assert_has_calls(
+            [
+                mock.call("test/source_rmse", approach.test_source_rmse),
+                mock.call("test/source_score", approach.test_source_score),
+            ]
+        )
+
+        approach.test_source_rmse.reset_mock()
+        approach.test_source_score.reset_mock()
+
+        # check target data loader call
+        approach.test_step([features, labels], batch_idx=0, dataloader_idx=1)
+        approach.test_source_rmse.assert_not_called()
+        approach.test_source_score.assert_not_called()
+        approach.test_target_rmse.assert_called_once()
+        approach.test_target_score.assert_called_once()
+        approach.log.assert_has_calls(
+            [
+                mock.call("test/target_rmse", approach.test_target_rmse),
+                mock.call("test/target_score", approach.test_target_score),
+            ]
+        )
+
+
+@pytest.mark.integration
+def test_on_dummy():
+    pl.seed_everything(42)
+
+    fd1 = rul_datasets.reader.DummyReader(fd=1)
+    dm = rul_datasets.RulDataModule(fd1, 16)
+
+    feature_extractor = model.CnnExtractor(1, [16], 10, fc_units=10)
+    regressor = model.FullyConnectedHead(10, [1], act_func_on_last_layer=False)
+
+    pre_approach = ConsistencyApproachPretraining(0.001)
+    pre_approach.set_model(feature_extractor, regressor)
+
+    trainer = pl.Trainer(
+        logger=False,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        enable_checkpointing=False,
+        max_epochs=10,
+    )
+    trainer.fit(pre_approach, dm)
+
+    fd2 = fd1.get_compatible(fd=2, percent_broken=0.8)
+    dm = rul_datasets.DomainAdaptionDataModule(
+        rul_datasets.RulDataModule(fd1, 16), rul_datasets.RulDataModule(fd2, 16)
+    )
+
+    disc = model.FullyConnectedHead(10, [1], act_func_on_last_layer=False)
+
+    approach = ConsistencyApproach(1.0, 0.001, 10)
+    approach.set_model(pre_approach.feature_extractor, pre_approach.regressor, disc)
+
+    trainer = pl.Trainer(
+        logger=False,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        enable_checkpointing=False,
+        max_epochs=10,
+    )
+    trainer.fit(approach, dm)
+    trainer.test(approach, dm)
