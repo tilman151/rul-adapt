a6b028b4083f3f0f7f5e9e3549493ec5fb2a517b
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-01-13 12:13:26 +0100

    feat: add factory functions

--- rul_adapt/construct/__init__.py
+++ rul_adapt/construct/__init__.py
@@ -1,3 +1,8 @@
+from .consistency.functional import (
+    get_consistency_dann_config,
+    get_consistency_dann,
+    consistency_dann_from_config,
+)
 from .lstm_dann.functional import (
     get_lstm_dann,
     get_lstm_dann_config,
