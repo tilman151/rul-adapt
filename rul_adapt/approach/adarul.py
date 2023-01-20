27dfa4b01681cd3d94c7efbc015de22e0e5926dd
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-01-20 14:00:42 +0100

    feat: add adarul approach

--- rul_adapt/approach/adarul.py
+++ rul_adapt/approach/adarul.py
@@ -1,9 +1,12 @@
-from typing import Tuple
+from typing import Tuple, Optional, Any, List
 
 import torch
 import torchmetrics
+from torch import nn
 
+import rul_adapt
 from rul_adapt.approach.abstract import AdaptionApproach
+from rul_adapt.model import FullyConnectedHead
 
 
 class AdaRulApproachPretraining(AdaptionApproach):
@@ -41,4 +44,208 @@ class AdaRulApproachPretraining(AdaptionApproach):
 
 
 class AdaRulApproach(AdaptionApproach):
-    pass
+
+    CHECKPOINT_MODELS = ["_domain_disc"]
+
+    _domain_disc: nn.Module
+
+    def __init__(self, lr: float):
+        super().__init__()
+
+        self.lr = lr
+
+        # training metrics
+        self.gan_loss = nn.BCEWithLogitsLoss()
+
+        # validation metrics
+        self.val_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_source_score = rul_adapt.loss.RULScore()
+        self.val_target_score = rul_adapt.loss.RULScore()
+
+        # testing metrics
+        self.test_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_source_score = rul_adapt.loss.RULScore()
+        self.test_target_score = rul_adapt.loss.RULScore()
+
+        self.save_hyperparameters()
+
+    def set_model(
+        self,
+        feature_extractor: nn.Module,
+        regressor: nn.Module,
+        domain_disc: Optional[nn.Module] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Set the feature extractor, regressor and domain discriminator for this approach.
+        The discriminator is not allowed to have an activation function on its last
+        layer and needs to use only a single output neuron.
+        Args:
+            feature_extractor: The feature extraction network.
+            regressor: The RUL regression network.
+            domain_disc: The domain discriminator network.
+        """
+        domain_disc = self._check_domain_disc(domain_disc)
+        super().set_model(feature_extractor, regressor, *args, **kwargs)
+        self._domain_disc = domain_disc
+
+    def _check_domain_disc(self, domain_disc: Optional[nn.Module]) -> nn.Module:
+        if domain_disc is None:
+            raise ValueError(
+                "No domain discriminator was set. This approach is unlikely to work."
+            )
+        if (
+            isinstance(domain_disc, FullyConnectedHead)
+            and domain_disc.act_func_on_last_layer
+        ):
+            raise ValueError(
+                "Domain discriminator has an activation function on its last layer. "
+                "This is not allowed due to torch.nn.BCEWithLogitsLoss being used as "
+                "its loss. Please set 'act_func_on_last_layer' to False."
+            )
+
+        return domain_disc
+
+    @property
+    def domain_disc(self):
+        """The domain discriminator network."""
+        if hasattr(self, "_domain_disc"):
+            return self._domain_disc
+        else:
+            raise RuntimeError("Domain disc used before 'set_model' was called.")
+
+    def configure_optimizers(self) -> List[torch.optim.Adam]:
+        return [
+            torch.optim.Adam(self.domain_disc.parameters(), self.lr),
+            torch.optim.Adam(self.feature_extractor.parameters(), self.lr),
+        ]
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Predict the RUL values for a batch of input features."""
+        return self.regressor(self.feature_extractor(inputs))
+
+    def training_step(
+        self, batch: List[torch.Tensor], batch_idx: int, optimizer_idx: int
+    ) -> torch.Tensor:
+        """
+        Execute one training step.
+        The `batch` argument is a list of three tensors representing the source
+        features, source labels and target features. Both types of features are fed
+        to the feature extractor. Then the regression loss for the source domain and
+        the DANN loss between domains is computed. The regression, DANN and combined
+        loss are logged.
+        TODO: Update
+        Args:
+            batch: A list of a source feature, source label and target feature tensors.
+            batch_idx: The index of the current batch.
+            optimizer_idx: The index of the optimizer the loss is going to be used in.
+        Returns:
+            The combined loss.
+        """
+        source, _, target = batch
+
+        if optimizer_idx == 0:
+            loss = self._get_disc_loss(source, target)
+            self.log("train/disc_loss", loss)
+        elif optimizer_idx == 1:
+            loss = self._get_gen_loss(target)
+            self.log("train/gen_loss", loss)
+        else:
+            raise RuntimeError("Too many optimizers.")
+
+        return loss
+
+    def _get_disc_loss(self, source, target):
+        batch_size = source.shape[0]
+        source = self.feature_extractor(source).detach()
+        target = self.feature_extractor(target).detach()
+        domain_pred = self._domain_disc(torch.cat([source, target]))
+        domain_labels = torch.cat(
+            [
+                torch.zeros(batch_size, 1, device=self.device),
+                torch.ones(batch_size, 1, device=self.device),
+            ]
+        )
+        loss = self.gan_loss(domain_pred, domain_labels)
+
+        return loss
+
+    def _get_gen_loss(self, target):
+        batch_size = target.shape[0]
+        target = self.feature_extractor(target)
+        domain_pred = self._domain_disc(target)
+        domain_labels = torch.zeros(batch_size, 1, device=self.device)
+        loss = -self.gan_loss(domain_pred, domain_labels)  # should maximize loss
+
+        return loss
+
+    def validation_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int
+    ) -> None:
+        """
+        Execute one validation step.
+
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `val`.
+
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        features, labels = batch
+        labels = labels[:, None]
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.val_source_rmse(predictions, labels)
+            self.val_source_score(predictions, labels)
+            self.log("val/source_rmse", self.val_source_rmse)
+            self.log("val/source_score", self.val_source_score)
+        elif dataloader_idx == 1:
+            self.val_target_rmse(predictions, labels)
+            self.val_target_score(predictions, labels)
+            self.log("val/target_rmse", self.val_target_rmse)
+            self.log("val/target_score", self.val_target_score)
+        else:
+            raise RuntimeError(f"Unexpected val data loader idx {dataloader_idx}")
+
+    def test_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int
+    ) -> None:
+        """
+        Execute one test step.
+
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `test`.
+
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        features, labels = batch
+        labels = labels[:, None]
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.test_source_rmse(predictions, labels)
+            self.test_source_score(predictions, labels)
+            self.log("test/source_rmse", self.test_source_rmse)
+            self.log("test/source_score", self.test_source_score)
+        elif dataloader_idx == 1:
+            self.test_target_rmse(predictions, labels)
+            self.test_target_score(predictions, labels)
+            self.log("test/target_rmse", self.test_target_rmse)
+            self.log("test/target_score", self.test_target_score)
+        else:
+            raise RuntimeError(f"Unexpected test data loader idx {dataloader_idx}")
--- tests/test_approach/test_adarul.py
+++ tests/test_approach/test_adarul.py
@@ -8,7 +8,8 @@ from torch import nn
 from torchmetrics import Metric
 
 from rul_adapt import model
-from rul_adapt.approach import AdaRulApproachPretraining
+from rul_adapt.approach import AdaRulApproachPretraining, AdaRulApproach
+from tests.test_approach import utils
 
 
 @pytest.fixture()
@@ -17,11 +18,23 @@ def pretraining_inputs():
 
 
 @pytest.fixture()
+def inputs(pretraining_inputs):
+    return *pretraining_inputs, torch.randn(10, 14, 20)
+
+
+@pytest.fixture()
 def pretraining_models():
-    fe = model.CnnExtractor(14, [10, 5], 20, fc_units=20)
-    reg = model.FullyConnectedHead(20, [10, 1], act_func_on_last_layer=False)
+    fe = model.LstmExtractor(14, [32, 32, 32], bidirectional=True)
+    reg = model.FullyConnectedHead(64, [32, 1], act_func_on_last_layer=False)
 
     return fe, reg
+
+
+@pytest.fixture()
+def models(pretraining_models):
+    disc = model.FullyConnectedHead(64, [32, 1], act_func_on_last_layer=False)
+
+    return *pretraining_models, disc
 
 
 @pytest.fixture()
@@ -33,11 +46,32 @@ def pretraining_approach(pretraining_models):
 
 
 @pytest.fixture()
+def approach(models):
+    approach = AdaRulApproach(0.001)
+    approach.set_model(*models)
+    approach.log = mock.MagicMock()
+
+    return approach
+
+
+@pytest.fixture()
 def mocked_pretraining_approach():
     feature_extractor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 20))
     regressor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
     approach = AdaRulApproachPretraining(0.001)
     approach.set_model(feature_extractor, regressor)
+
+    return approach
+
+
+@pytest.fixture()
+def mocked_approach():
+    feature_extractor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 20))
+    regressor = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
+    disc = mock.MagicMock(nn.Module, return_value=torch.zeros(10, 1))
+    approach = AdaRulApproach(0.001)
+    approach.set_model(feature_extractor, regressor, disc)
+    approach.log = mock.MagicMock()
 
     return approach
 
@@ -87,24 +121,240 @@ class TestAdaRulPretraining:
         approach.val_loss.assert_called_once()
         approach.log.assert_called_with("val/loss", approach.val_loss)
 
-    @pytest.mark.integration
-    def test_on_dummy(self):
-        pl.seed_everything(42)
 
-        fd1 = rul_datasets.reader.DummyReader(fd=1)
-        dm = rul_datasets.RulDataModule(fd1, 16)
-
-        feature_extractor = model.CnnExtractor(1, [16], 10, fc_units=10)
-        regressor = model.FullyConnectedHead(10, [1], act_func_on_last_layer=False)
-
-        approach = AdaRulApproachPretraining(0.001)
-        approach.set_model(feature_extractor, regressor)
-
-        trainer = pl.Trainer(
-            logger=False,
-            enable_progress_bar=False,
-            enable_model_summary=False,
-            enable_checkpointing=False,
-            max_epochs=10,
+class TestAdaRulApproach:
+    def test_set_model(self, pretraining_approach, approach, models):
+        _, _, domain_disc = models
+        approach.set_model(
+            pretraining_approach.feature_extractor,
+            pretraining_approach.regressor,
+            domain_disc,
         )
-        trainer.fit(approach, dm)
+
+        assert approach.feature_extractor is pretraining_approach.feature_extractor
+        assert approach.regressor is pretraining_approach.regressor
+        assert hasattr(approach, "_domain_disc")  # domain_disc was assigned
+        assert approach.domain_disc is domain_disc  # domain_disc property works
+
+    def test_domain_disc_check(self, models):
+        feature_extractor, regressor, _ = models
+        faulty_domain_disc = model.FullyConnectedHead(
+            20, [1], act_func_on_last_layer=True
+        )
+        approach = AdaRulApproach(0.01)
+
+        with pytest.raises(ValueError):
+            approach.set_model(feature_extractor, regressor)
+
+        with pytest.raises(ValueError):
+            approach.set_model(feature_extractor, regressor, faulty_domain_disc)
+
+    def test_forward(self, inputs, approach):
+        features, *_ = inputs
+
+        outputs = approach(features)
+
+        assert outputs.shape == torch.Size([10, 1])
+
+    def test_train_step(self, inputs, approach):
+        outputs = approach.training_step(inputs, batch_idx=0, optimizer_idx=0)
+        assert outputs.shape == torch.Size([])
+
+        outputs = approach.training_step(inputs, batch_idx=0, optimizer_idx=1)
+        assert outputs.shape == torch.Size([])
+
+        with pytest.raises(RuntimeError):
+            approach.training_step(inputs, batch_idx=0, optimizer_idx=2)
+
+    def test_configure_optimizer(self, approach):
+        disc_optim, gen_optim = approach.configure_optimizers()
+
+        assert isinstance(disc_optim, torch.optim.Adam)
+        assert disc_optim.param_groups[0]["params"] == list(
+            approach.domain_disc.parameters()
+        )
+
+        assert isinstance(gen_optim, torch.optim.Adam)
+        assert gen_optim.param_groups[0]["params"] == list(
+            approach.feature_extractor.parameters()
+        )
+
+    def test_train_step_backward_disc(self, inputs, approach):
+        """Feature extractor should have no gradients in disc step. Disc should have
+        gradients."""
+        outputs = approach.training_step(inputs, batch_idx=0, optimizer_idx=0)
+        outputs.backward()
+
+        for param in approach.feature_extractor.parameters():
+            assert param.grad is None
+        for param in approach.domain_disc.parameters():
+            assert param.grad is not None
+
+    def test_train_step_backward_gen(self, inputs, approach):
+        """Feature extractor should have gradients in gen step. Disc will necessarily
+        have gradients but this is unimportant here."""
+        outputs = approach.training_step(inputs, batch_idx=0, optimizer_idx=1)
+        outputs.backward()
+
+        for param in approach.feature_extractor.parameters():
+            assert param.grad is not None
+
+    @torch.no_grad()
+    def test_val_step(self, inputs, approach):
+        features, labels, _ = inputs
+
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=0)
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=1)
+        with pytest.raises(RuntimeError):
+            approach.validation_step([features, labels], batch_idx=0, dataloader_idx=2)
+
+    @torch.no_grad()
+    def test_train_step_disc_logging(self, inputs, mocked_approach):
+        approach = mocked_approach
+        approach.gan_loss = mock.MagicMock(nn.Module)
+
+        approach.training_step(inputs, batch_idx=0, optimizer_idx=0)
+
+        approach.gan_loss.assert_called_once()
+        approach.log.assert_called_with("train/disc_loss", approach.gan_loss())
+
+    @torch.no_grad()
+    def test_train_step_gen_logging(self, inputs, mocked_approach):
+        approach = mocked_approach
+        approach.gan_loss = mock.MagicMock(nn.Module)
+
+        approach.training_step(inputs, batch_idx=0, optimizer_idx=1)
+
+        approach.gan_loss.assert_called_once()
+        approach.log.assert_called_with("train/gen_loss", -approach.gan_loss())
+
+    @torch.no_grad()
+    def test_val_step_logging(self, mocked_approach, inputs):
+        approach = mocked_approach
+
+        features, labels, _ = inputs
+        approach.val_source_rmse = mock.MagicMock(Metric)
+        approach.val_source_score = mock.MagicMock(Metric)
+        approach.val_target_rmse = mock.MagicMock(Metric)
+        approach.val_target_score = mock.MagicMock(Metric)
+
+        # check source data loader call
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=0)
+        approach.val_source_rmse.assert_called_once()
+        approach.val_source_score.assert_called_once()
+        approach.val_target_rmse.assert_not_called()
+        approach.val_target_score.assert_not_called()
+        approach.log.assert_has_calls(
+            [
+                mock.call("val/source_rmse", approach.val_source_rmse),
+                mock.call("val/source_score", approach.val_source_score),
+            ]
+        )
+
+        approach.val_source_rmse.reset_mock()
+        approach.val_source_score.reset_mock()
+
+        # check target data loader call
+        approach.validation_step([features, labels], batch_idx=0, dataloader_idx=1)
+        approach.val_source_rmse.assert_not_called()
+        approach.val_source_score.assert_not_called()
+        approach.val_target_rmse.assert_called_once()
+        approach.val_target_score.assert_called_once()
+        approach.log.assert_has_calls(
+            [
+                mock.call("val/target_rmse", approach.val_target_rmse),
+                mock.call("val/target_score", approach.val_target_score),
+            ]
+        )
+
+    @torch.no_grad()
+    def test_test_step_logging(self, mocked_approach, inputs):
+        approach = mocked_approach
+        features, labels, _ = inputs
+        approach.test_source_rmse = mock.MagicMock(Metric)
+        approach.test_source_score = mock.MagicMock(Metric)
+        approach.test_target_rmse = mock.MagicMock(Metric)
+        approach.test_target_score = mock.MagicMock(Metric)
+
+        # check source data loader call
+        approach.test_step([features, labels], batch_idx=0, dataloader_idx=0)
+        approach.test_source_rmse.assert_called_once()
+        approach.test_source_score.assert_called_once()
+        approach.test_target_rmse.assert_not_called()
+        approach.test_target_score.assert_not_called()
+        approach.log.assert_has_calls(
+            [
+                mock.call("test/source_rmse", approach.test_source_rmse),
+                mock.call("test/source_score", approach.test_source_score),
+            ]
+        )
+
+        approach.test_source_rmse.reset_mock()
+        approach.test_source_score.reset_mock()
+
+        # check target data loader call
+        approach.test_step([features, labels], batch_idx=0, dataloader_idx=1)
+        approach.test_source_rmse.assert_not_called()
+        approach.test_source_score.assert_not_called()
+        approach.test_target_rmse.assert_called_once()
+        approach.test_target_score.assert_called_once()
+        approach.log.assert_has_calls(
+            [
+                mock.call("test/target_rmse", approach.test_target_rmse),
+                mock.call("test/target_score", approach.test_target_score),
+            ]
+        )
+
+    def test_checkpointing(self, tmp_path):
+        ckpt_path = tmp_path / "checkpoint.ckpt"
+        fe = model.CnnExtractor(1, [16], 10, fc_units=16)
+        reg = model.FullyConnectedHead(16, [1])
+        disc = model.FullyConnectedHead(16, [1], act_func_on_last_layer=False)
+        approach = AdaRulApproach(0.01)
+        approach.set_model(fe, reg, disc)
+
+        utils.checkpoint(approach, ckpt_path)
+        AdaRulApproach.load_from_checkpoint(ckpt_path)
+
+
+@pytest.mark.integration
+def test_on_dummy():
+    pl.seed_everything(42)
+
+    fd1 = rul_datasets.reader.DummyReader(fd=1)
+    dm = rul_datasets.RulDataModule(fd1, 16)
+
+    feature_extractor = model.LstmExtractor(1, [10], bidirectional=True)
+    regressor = model.FullyConnectedHead(20, [1], act_func_on_last_layer=False)
+
+    pre_approach = AdaRulApproachPretraining(0.0001)
+    pre_approach.set_model(feature_extractor, regressor)
+
+    trainer = pl.Trainer(
+        logger=False,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        enable_checkpointing=False,
+        max_epochs=10,
+    )
+    trainer.fit(pre_approach, dm)
+
+    fd2 = fd1.get_compatible(fd=2, percent_broken=0.8)
+    dm = rul_datasets.DomainAdaptionDataModule(
+        rul_datasets.RulDataModule(fd1, 16), rul_datasets.RulDataModule(fd2, 16)
+    )
+
+    disc = model.FullyConnectedHead(20, [1], act_func_on_last_layer=False)
+
+    approach = AdaRulApproach(0.0001)
+    approach.set_model(pre_approach.feature_extractor, pre_approach.regressor, disc)
+
+    trainer = pl.Trainer(
+        logger=False,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        enable_checkpointing=False,
+        max_epochs=10,
+    )
+    trainer.fit(approach, dm)
+    trainer.test(approach, dm)
