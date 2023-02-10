822ebbf6cd4d388f0a7cf80aa1f35338cb0c04a4
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-02-10 13:35:06 +0100

    feat: add tbigru approach

---
+++ rul_adapt/approach/tbigru.py
@@ -0,0 +1,307 @@
+from itertools import product
+from typing import List, Tuple, Optional
+
+import numpy as np
+import pywt  # type: ignore
+import rul_datasets.reader.scaling
+import torch
+import torchmetrics
+from dtaidistance import dtw  # type: ignore
+from rul_datasets.reader import AbstractReader
+from scipy.stats import wasserstein_distance  # type: ignore
+from sklearn.preprocessing import MinMaxScaler
+
+import rul_adapt
+from rul_adapt.approach.abstract import AdaptionApproach
+
+
+def rms(inputs: np.ndarray) -> np.ndarray:
+    return np.sqrt(np.mean(inputs**2, axis=-2))
+
+
+def p2p(inputs: np.ndarray) -> np.ndarray:
+    return np.max(inputs, axis=-2) - np.min(inputs, axis=-2)
+
+
+def skewness(inputs: np.ndarray) -> np.ndarray:
+    sdev = inputs.std(axis=-2)
+    mean = inputs.mean(axis=-2, keepdims=True)
+    skew = np.sum((inputs - mean) ** 3, axis=-2) / (inputs.shape[-2] * sdev**3)
+
+    return skew
+
+
+def impulse_factor(inputs: np.ndarray) -> np.ndarray:
+    absolute = np.abs(inputs)
+    imp_f = np.max(absolute, axis=-2) / np.mean(absolute, axis=-2)
+
+    return imp_f
+
+
+def median_absolute(inputs: np.ndarray) -> np.ndarray:
+    return np.median(np.abs(inputs), axis=-2)
+
+
+def mean_absolute(inputs: np.ndarray) -> np.ndarray:
+    return np.mean(np.abs(inputs), axis=-2)
+
+
+def max_absolute(inputs: np.ndarray) -> np.ndarray:
+    return np.max(np.abs(inputs), axis=-2)
+
+
+def kurtosis(inputs: np.ndarray) -> np.ndarray:
+    sdev = inputs.std()
+    mean = inputs.mean(axis=-2, keepdims=True)
+    kurt = np.sum((inputs - mean) ** 4, axis=-2) / (inputs.shape[-2] * sdev**2)
+
+    return kurt
+
+
+def std(inputs: np.ndarray) -> np.ndarray:
+    return np.std(inputs, axis=-2)
+
+
+def margin_factor(inputs: np.ndarray) -> np.ndarray:
+    absolute = np.abs(inputs)
+    mf = np.max(absolute, axis=-2) / (np.mean(np.sqrt(absolute), axis=-2)) ** 2
+
+    return mf
+
+
+def energy(inputs: np.ndarray) -> np.ndarray:
+    return np.sum(inputs**2, axis=-2)
+
+
+def gini_factor(inputs: np.ndarray) -> np.ndarray:
+    batched = len(inputs.shape) == 3
+    inputs = inputs if batched else inputs[None]
+    gini = _approx_batched_gini_factor(inputs)
+    gini = gini if batched else gini.squeeze(0)
+
+    return gini
+
+
+def _approx_batched_gini_factor(inputs: np.ndarray) -> np.ndarray:
+    window_size = inputs.shape[1]
+    inputs = np.sort(inputs, axis=1)
+    cumsum = np.cumsum(inputs, axis=1)
+    gini = (window_size + 1 - 2 * np.sum(cumsum, axis=1) / cumsum[:, -1]) / window_size
+
+    return gini
+
+
+def band_energies(inputs: np.ndarray) -> np.ndarray:
+    wp = pywt.WaveletPacket(inputs, wavelet="db1", maxlevel=4, axis=-2)
+    bands = (node.data for node in wp.get_level(4))
+    energies = np.concatenate([energy(b) for b in bands], axis=-1)
+
+    return energies
+
+
+def std_ihc(inputs: np.ndarray) -> np.ndarray:
+    return np.std(np.log(inputs + np.sqrt(inputs**2 - 1)), axis=-2)
+
+
+def std_ihs(inputs: np.ndarray) -> np.ndarray:
+    return np.std(np.log(inputs + np.sqrt(inputs**2 + 1)), axis=-2)
+
+
+class VibrationFeatureExtractor:
+    def __init__(
+        self, num_input_features: int, feature_idx: Optional[List[int]] = None
+    ) -> None:
+        self.num_input_features = num_input_features
+        self.feature_idx = list(range(60)) if feature_idx is None else feature_idx
+
+        if min(self.feature_idx) < 0 or max(self.feature_idx) > 60:
+            raise ValueError("Feature indices need to be between 0 and 60.")
+
+        self._scaler = None
+
+    def __call__(
+        self, features: np.ndarray, targets: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        features = self._extract_selected(features)
+        if self._scaler is not None:
+            features = self._scaler.transform(features)
+
+        return features, targets
+
+    def _extract_selected(self, features: np.ndarray) -> np.ndarray:
+        return _extract_all(features, self.num_input_features)[:, self.feature_idx]
+
+    def fit(self, features: List[np.ndarray]) -> "VibrationFeatureExtractor":
+        features = [self._extract_selected(f) for f in features]
+        self._scaler = MinMaxScaler()
+        for feat in features:
+            self._scaler.partial_fit(feat)
+
+        return self
+
+
+def _extract_all(features: np.ndarray, num_features: int) -> np.ndarray:
+    features = [
+        rms(features),
+        kurtosis(features),
+        p2p(features),
+        std(features),
+        skewness(features),
+        margin_factor(features),
+        impulse_factor(features),
+        energy(features),
+        median_absolute(features),
+        gini_factor(features),
+        max_absolute(features),
+        mean_absolute(features),
+        *band_energies(features)
+        .reshape((-1, 16, num_features))
+        .transpose(1, 0, 2),  # unpack features
+        std_ihc(features),
+        std_ihs(features),
+    ]
+    features = np.concatenate(features, axis=1)
+
+    return features
+
+
+def domain_distance(source: np.ndarray, target: np.ndarray, ratio=0.4) -> float:
+    dtw_dist = dtw.distance_fast(source, target)
+    wasserstein_dist = wasserstein_distance(source, target)
+    distance = ratio * dtw_dist + (1 - ratio) * wasserstein_dist
+
+    return distance
+
+
+def select_features(
+    source: AbstractReader, target: AbstractReader, num_features: int
+) -> List[int]:
+    source_runs, _ = source.load_split("dev")
+    target_runs, _ = target.load_split("dev")
+
+    num_org_features = source_runs[0].shape[-1]
+    distances = np.zeros(30 * num_org_features)
+    for source_run, target_run in product(source_runs, target_runs):
+        source_feats = _extract_all(source_run, num_features=num_org_features)
+        target_feats = _extract_all(target_run, num_features=num_org_features)
+        for i, (s, f) in enumerate(zip(source_feats.T, target_feats.T)):
+            distances[i] += domain_distance(s, f)
+
+    feature_idx = np.argsort(distances)[:num_features]
+
+    return feature_idx
+
+
+class TBiGruApproach(AdaptionApproach):
+    def __init__(self, lr: float, mmd_factor: float):
+        super().__init__()
+
+        self.lr = lr
+        self.mmd_factor = mmd_factor
+
+        # training metrics
+        self.train_source_loss = torchmetrics.MeanSquaredError()
+        self.mmd_loss = rul_adapt.loss.MaximumMeanDiscrepancyLoss(num_kernels=5)
+
+        # validation metrics
+        self.val_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.val_source_score = rul_adapt.loss.RULScore()
+        self.val_target_score = rul_adapt.loss.RULScore()
+
+        # testing metrics
+        self.test_source_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_target_rmse = torchmetrics.MeanSquaredError(squared=False)
+        self.test_source_score = rul_adapt.loss.RULScore()
+        self.test_target_score = rul_adapt.loss.RULScore()
+
+        self.save_hyperparameters()
+
+    def configure_optimizers(self) -> torch.optim.Adam:
+        return torch.optim.Adam(self.parameters(), self.lr)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Predict the RUL values for a batch of input features."""
+        return self.regressor(self.feature_extractor(inputs))
+
+    def training_step(self, batch: List[torch.Tensor], batch_idx: int) -> torch.Tensor:
+        source, source_labels, target = batch
+        source_labels = source_labels[:, None]
+
+        source = self.feature_extractor(source)
+        target = self.feature_extractor(target)
+        rul_preds = self.regressor(source)
+
+        mse_loss = self.train_source_loss(rul_preds, source_labels)
+        mmd_loss = self.mmd_loss(source, target)
+        loss = mse_loss + self.mmd_factor * mmd_loss
+
+        self.log("train/loss", loss)
+        self.log("train/source_loss", self.train_source_loss)
+        self.log("train/mmd", self.mmd_loss)
+
+        return loss
+
+    def validation_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int
+    ) -> None:
+        """
+        Execute one validation step.
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `val`.
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        features, labels = batch
+        labels = labels[:, None]
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.val_source_rmse(predictions, labels)
+            self.val_source_score(predictions, labels)
+            self.log("val/source_rmse", self.val_source_rmse)
+            self.log("val/source_score", self.val_source_score)
+        elif dataloader_idx == 1:
+            self.val_target_rmse(predictions, labels)
+            self.val_target_score(predictions, labels)
+            self.log("val/target_rmse", self.val_target_rmse)
+            self.log("val/target_score", self.val_target_score)
+        else:
+            raise RuntimeError(f"Unexpected val data loader idx {dataloader_idx}")
+
+    def test_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int
+    ) -> None:
+        """
+        Execute one test step.
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `test`.
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        features, labels = batch
+        labels = labels[:, None]
+        predictions = self.forward(features)
+        if dataloader_idx == 0:
+            self.test_source_rmse(predictions, labels)
+            self.test_source_score(predictions, labels)
+            self.log("test/source_rmse", self.test_source_rmse)
+            self.log("test/source_score", self.test_source_score)
+        elif dataloader_idx == 1:
+            self.test_target_rmse(predictions, labels)
+            self.test_target_score(predictions, labels)
+            self.log("test/target_rmse", self.test_target_rmse)
+            self.log("test/target_score", self.test_target_score)
+        else:
+            raise RuntimeError(f"Unexpected test data loader idx {dataloader_idx}")
---
+++ tests/test_approach/test_tbigru.py
@@ -0,0 +1,236 @@
+import numpy as np
+import numpy.testing as npt
+import pytest
+import rul_datasets
+import pytorch_lightning as pl
+import torch
+from rul_datasets import utils
+
+import rul_adapt.model
+from rul_adapt.approach import tbigru
+
+
+@pytest.fixture(params=[(5000, 2), (10, 5000, 2)], ids=["unbatched", "batched"])
+def inputs_normal(request):
+    return np.random.default_rng(seed=42).standard_normal(request.param)
+
+
+@pytest.fixture(params=[(5000, 2), (10, 5000, 2)], ids=["unbatched", "batched"])
+def inputs_uniform(request):
+    return np.random.default_rng(seed=42).uniform(-1, 1, request.param)
+
+
+def test_rms(inputs_normal):
+    rms = tbigru.rms(inputs_normal)
+    _assert_shape(rms)
+
+
+def test_p2p(inputs_uniform):
+    p2p = tbigru.p2p(inputs_uniform)
+    _assert_shape(p2p)
+    npt.assert_almost_equal(p2p, 2, decimal=2)
+
+
+def test_skewness(inputs_normal):
+    skew = tbigru.skewness(inputs_normal)
+    _assert_shape(skew)
+    npt.assert_almost_equal(skew, 0, decimal=1)  # value for standard normal
+
+
+def test_kurtosis(inputs_normal):
+    kurt = tbigru.kurtosis(inputs_normal)
+    _assert_shape(kurt)
+    npt.assert_almost_equal(kurt, 3, decimal=0)  # value for standard normal
+
+
+def test_impulse_factor(inputs_uniform):
+    imp_f = tbigru.impulse_factor(inputs_uniform)
+    _assert_shape(imp_f)
+    npt.assert_almost_equal(imp_f, 2, decimal=1)  # max abs of 1 over mean abs of 0.5
+
+
+def test_median_absolute(inputs_uniform):
+    median_abs = tbigru.median_absolute(inputs_uniform)
+    _assert_shape(median_abs)
+    npt.assert_almost_equal(median_abs, 0.5, decimal=1)
+
+
+def test_mean_absolute(inputs_uniform):
+    mean_abs = tbigru.mean_absolute(inputs_uniform)
+    _assert_shape(mean_abs)
+    npt.assert_almost_equal(mean_abs, 0.5, decimal=1)
+
+
+def test_max_absolute(inputs_uniform):
+    max_abs = tbigru.max_absolute(inputs_uniform)
+    _assert_shape(max_abs)
+    npt.assert_almost_equal(max_abs, 1, decimal=1)
+
+
+def test_std(inputs_normal):
+    std = tbigru.std(inputs_normal)
+    _assert_shape(std)
+    npt.assert_almost_equal(std, 1, decimal=1)
+
+
+def test_margin_factor(inputs_uniform):
+    mf = tbigru.margin_factor(inputs_uniform)
+    _assert_shape(mf)
+    npt.assert_almost_equal(mf, 2.3, decimal=1)
+
+
+def test_energy(inputs_normal):
+    energy = tbigru.energy(inputs_normal)
+    _assert_shape(energy)
+
+
+def test_gini_factor(inputs_uniform):
+    gini = tbigru.gini_factor(inputs_uniform)
+    _assert_shape(gini)
+
+
+@pytest.mark.parametrize(
+    "inputs",
+    [
+        np.random.default_rng(seed=42).random((10, 250, 2)),
+        np.random.default_rng(seed=42).uniform(-1, 1, (10, 250, 2)),
+    ],
+)
+def test_gini_factor_approximation(inputs):
+    gini = tbigru.gini_factor(inputs)
+    accurate_gini = _accurate_gini_factor(inputs)
+    npt.assert_almost_equal(gini, accurate_gini)
+
+
+def _accurate_gini_factor(inputs: np.ndarray) -> np.ndarray:
+    """Infeasible due to O(n**2) memory requirements."""
+    abs_mean_diff = np.mean(
+        np.abs(inputs[:, :, None, :] - inputs[:, None, :, :]), axis=(1, 2)
+    )
+    rel_mean_diff = abs_mean_diff / np.mean(inputs, axis=1)
+    gini = rel_mean_diff / 2
+
+    return gini
+
+
+def test_band_energies(inputs_normal):
+    energies = tbigru.band_energies(inputs_normal)
+    _assert_shape(energies, multiplier=16)
+
+
+def test_std_ihc(inputs_normal):
+    ihc = tbigru.std_ihc(inputs_normal)
+    _assert_shape(ihc)
+
+
+def test_std_ihs(inputs_normal):
+    ihs = tbigru.std_ihs(inputs_normal)
+    _assert_shape(ihs)
+
+
+def _assert_shape(outputs, multiplier=1):
+    batched = len(outputs.shape) == 2
+    expected_shape = (10, multiplier * 2) if batched else (multiplier * 2,)
+    assert outputs.shape == expected_shape
+
+
+@pytest.mark.parametrize("feature_idx", [[0, 1, 59], None])
+def test_feature_extractor(feature_idx):
+    inputs = np.random.randn(10, 2560, 2)
+    targets = np.arange(10)
+    extractor = tbigru.VibrationFeatureExtractor(2, feature_idx)
+
+    outputs, _ = extractor(inputs, targets)
+
+    num_features = len(feature_idx or range(60))
+    assert outputs.shape == (10, num_features)
+
+
+def test_feature_extractor_selection():
+    inputs = np.random.randn(10, 2560, 2)
+    targets = np.arange(10)
+    extractor = tbigru.VibrationFeatureExtractor(2, [1, 25, 59])
+
+    outputs, _ = extractor(inputs, targets)
+
+    npt.assert_almost_equal(outputs[:, 0], tbigru.rms(inputs)[:, 1])
+    npt.assert_almost_equal(outputs[:, 1], tbigru.band_energies(inputs)[:, 1])
+    npt.assert_almost_equal(outputs[:, 2], tbigru.std_ihs(inputs)[:, 1])
+
+
+def test_feature_extractor_as_scaler():
+    inputs = np.random.randn(10, 2560, 2)
+    targets = np.arange(10)
+    extractor = tbigru.VibrationFeatureExtractor(2, [0, 1, 20, 40])
+    extractor.fit([inputs])
+
+    outputs, _ = extractor(inputs, targets)
+
+    assert np.max(outputs) <= 1.0
+    assert np.min(outputs) >= 0.0
+
+
+def test_domain_distance():
+    source = np.sin(np.linspace(0, 10, 2560))
+    target_close = np.sin(2 * np.linspace(0, 10, 2560)) + np.random.randn(2560) * 1e-4
+    target_far = np.sin(3 * np.linspace(0, 10, 2560)) + np.random.randn(2560) * 1e-2
+
+    distance = tbigru.domain_distance(source, source)
+    assert distance == 0.0
+
+    distance_close = tbigru.domain_distance(source, target_close)
+    distance_far = tbigru.domain_distance(source, target_far)
+    assert distance_close < distance_far
+
+
+def test_feature_selection():
+    source = rul_datasets.reader.DummyReader(1)
+    target = rul_datasets.reader.DummyReader(2)
+
+    feature_idx = tbigru.select_features(source, target, num_features=15)
+
+    assert len(feature_idx) == 15
+    assert max(feature_idx) < 30
+    assert min(feature_idx) >= 0
+
+
+@pytest.mark.integration
+def test_on_dummy():
+    torch.autograd.set_detect_anomaly(True)
+    source = rul_datasets.reader.DummyReader(1)
+    target = source.get_compatible(2, percent_broken=0.8)
+    feature_idx = tbigru.select_features(source, target, num_features=15)
+    source_extractor = tbigru.VibrationFeatureExtractor(1, feature_idx)
+    source_extractor.fit(source.load_split("dev")[0] + target.load_split("dev")[0])
+    dm = rul_datasets.DomainAdaptionDataModule(
+        rul_datasets.RulDataModule(source, 32, source_extractor, 20),
+        rul_datasets.RulDataModule(target, 32, source_extractor, 20),
+    )
+
+    fe = rul_adapt.model.CnnExtractor(15, [16, 16], 20, fc_units=16)
+    reg = rul_adapt.model.FullyConnectedHead(16, [1], act_func_on_last_layer=False)
+    approach = tbigru.TBiGruApproach(0.001, 0.1)
+    approach.set_model(fe, reg)
+
+    # workaround because test split has only one window by default
+    def _truncate_test_split(rng, features, targets):
+        for i in range(len(features)):
+            run_len = len(features[i])
+            cutoff = rng.integers(run_len // 2, run_len - 1)
+            features[i] = features[i][:cutoff]
+            targets[i] = targets[i][:cutoff]
+
+        return features, targets
+
+    source._truncate_test_split = _truncate_test_split
+    target._truncate_test_split = _truncate_test_split
+
+    trainer = pl.Trainer(
+        logger=False,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        enable_checkpointing=False,
+        max_epochs=10,
+    )
+    trainer.fit(approach, dm)
+    trainer.test(approach, dm)
