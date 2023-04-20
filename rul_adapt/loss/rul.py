98e82bd53189c982f45515051777b9f696b4c480
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-04-20 12:06:13 +0200

    feat: add PHM12 mode to RUL score

--- rul_adapt/loss/rul.py
+++ rul_adapt/loss/rul.py
@@ -1,35 +1,64 @@
-from typing import Any
+import math
+from typing import Any, Literal, List, Optional
 
 import torch
 import torchmetrics
 
+from rul_adapt.loss.utils import weighted_mean
+
 
 class RULScore(torchmetrics.Metric):
     is_differentiable = True
-    higher_is_better = False
+    higher_is_better = None
     full_state_update = False
 
-    loss: torch.Tensor
-    total: torch.Tensor
+    loss: List[torch.Tensor]
+    total: List[torch.Tensor]
 
-    def __init__(self, pos_factor=10.0, neg_factor=-13.0, mean: bool = False):
+    def __init__(
+        self, mode: Literal["phm08", "phm12"] = "phm08", mean: Optional[bool] = None
+    ):
         super().__init__()
-        self.pos_factor = pos_factor
-        self.neg_factor = neg_factor
-        self.mean = mean
+        if mode == "phm08":
+            self.pos_factor = 10.0
+            self.neg_factor = -13.0
+            self.offset = -1.0
+            self.as_percentage = False
+            self.mean = False
+        elif mode == "phm12":
+            self.pos_factor = 20 / math.log(0.5)
+            self.neg_factor = -5 / math.log(0.5)
+            self.offset = 0.0
+            self.as_percentage = True
+            self.mean = True
+        else:
+            raise ValueError(f"Unknown RUL score mode: {mode}")
 
-        self.add_state("loss", default=torch.tensor(0.0), dist_reduce_fx="sum")
-        self.add_state("total", default=torch.tensor(0.0), dist_reduce_fx="sum")
+        if mean is not None:
+            self.mean = mean
+
+        self.add_state("loss", [], dist_reduce_fx="cat")
+        self.add_state("total", [], dist_reduce_fx="cat")
 
     def update(self, inputs: torch.Tensor, targets: torch.Tensor) -> None:
-        self.loss += rul_score(inputs, targets, self.pos_factor, self.neg_factor)
-        self.total += inputs.shape[0]
+        self.loss.append(
+            rul_score(
+                inputs,
+                targets,
+                self.pos_factor,
+                self.neg_factor,
+                self.offset,
+                self.as_percentage,
+            )
+        )
+        self.total.append(torch.tensor(inputs.shape[0], device=self.device))
 
     def compute(self) -> Any:
         if self.mean:
-            return self.loss / self.total
+            loss = [l / t for l, t in zip(self.loss, self.total)]
+            return weighted_mean(loss, self.total)
         else:
-            return self.loss
+            return sum(self.loss)
 
 
 def rul_score(
@@ -37,12 +66,16 @@ def rul_score(
     targets: torch.Tensor,
     pos_factor: float,
     neg_factor: float,
+    offset: float,
+    as_percentage: bool,
 ) -> torch.Tensor:
     dist = inputs - targets
+    if as_percentage:
+        dist = dist / targets * 100
     factors = torch.ones_like(dist)
     factors[dist >= 0] /= pos_factor
     factors[dist < 0] /= neg_factor
-    dist = torch.exp(dist * factors) - 1
+    dist = torch.exp(dist * factors) + offset
     score = dist.sum()
 
     return score
--- tests/test_loss/test_rul.py
+++ tests/test_loss/test_rul.py
@@ -14,16 +14,31 @@ def test_rul_score_functional(inputs, targets, exp_result):
     inputs = torch.tensor([inputs] * 10)
     targets = torch.tensor([targets] * 10)
 
-    score = loss.rul_score(inputs, targets, pos_factor=10.0, neg_factor=-13.0)
+    score = loss.rul_score(
+        inputs,
+        targets,
+        pos_factor=10.0,
+        neg_factor=-13.0,
+        offset=-1.0,
+        as_percentage=False,
+    )
 
     npt.assert_almost_equal(score, exp_result, decimal=5)
 
 
-def test_rul_score_functional_backward():
+@pytest.mark.parametrize("as_percentage", [True, False])
+def test_rul_score_functional_backward(as_percentage):
     inputs = torch.randn(10, 1, requires_grad=True)
     targets = torch.randn(10, 1)
 
-    score = loss.rul_score(inputs, targets, pos_factor=10.0, neg_factor=-13.0)
+    score = loss.rul_score(
+        inputs,
+        targets,
+        pos_factor=10.0,
+        neg_factor=-13.0,
+        offset=-1.0,
+        as_percentage=as_percentage,
+    )
     score.backward()
 
     assert inputs.grad is not None
@@ -34,21 +49,47 @@ def test_rul_score_functional_backward():
     ["inputs", "targets"],
     [(0.0, 0.0), (1.0, 1.0), (0.5, 1.0), (1.5, 1.0)],
 )
-def test_rul_score_modular(inputs, targets):
-    pos_factor = 10.0
-    neg_factor = -13.0
-    inputs = torch.tensor([inputs] * 50)
-    targets = torch.tensor([targets] * 50)
-    score_module = loss.RULScore(pos_factor, neg_factor)
+@pytest.mark.parametrize("mode", ["phm08", "phm12"])
+def test_rul_score_modular(inputs, targets, mode):
+    inputs = torch.tensor([inputs] * 52) + torch.randn(52) * 0.0001
+    targets = torch.tensor([targets] * 52)
+    score_module = loss.RULScore(mode)
 
     for batch in zip(torch.split(inputs, 10), torch.split(targets, 10)):
         module_score = score_module(*batch)
-        functional_score = loss.rul_score(*batch, pos_factor, neg_factor)
+        functional_score = loss.rul_score(
+            *batch,
+            score_module.pos_factor,
+            score_module.neg_factor,
+            score_module.offset,
+            score_module.as_percentage
+        )
+        if mode == "phm12":
+            functional_score /= batch[0].shape[0]
         npt.assert_almost_equal(module_score, functional_score)
 
-    total_functional_score = loss.rul_score(inputs, targets, pos_factor, neg_factor)
+    total_functional_score = loss.rul_score(
+        inputs,
+        targets,
+        score_module.pos_factor,
+        score_module.neg_factor,
+        score_module.offset,
+        score_module.as_percentage,
+    )
+    if mode == "phm12":
+        total_functional_score /= inputs.shape[0]
     total_module_score = score_module.compute()
-    npt.assert_almost_equal(total_module_score, total_functional_score)
+    npt.assert_almost_equal(total_module_score, total_functional_score, decimal=5)
+
+
+@pytest.mark.parametrize(["mode", "default"], [("phm08", False), ("phm12", True)])
+def test_mean_override(mode, default):
+    score_module = loss.RULScore(mode)
+    assert score_module.mean == default
+
+    for mean in [True, False]:
+        score_module = loss.RULScore(mode, mean=mean)
+        assert score_module.mean == mean
 
 
 def test_rul_score_modular_backward():
