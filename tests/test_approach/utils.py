c3aa8ed30b6e96e3e7c9d9b5ef4c1bd3b5d5e9cd
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-07-04 14:50:41 +0200

    tests: disable lightning logging in unit tests (#38)

--- tests/test_approach/utils.py
+++ tests/test_approach/utils.py
@@ -9,7 +9,7 @@ def checkpoint(approach, ckpt_path, max_rul=50):
     dm = rul_datasets.RulDataModule(
         rul_datasets.reader.DummyReader(1, max_rul=max_rul), 32
     )
-    trainer = pl.Trainer(max_epochs=0, num_sanity_val_steps=0)
+    trainer = pl.Trainer(max_epochs=0, num_sanity_val_steps=0, logger=False)
     trainer.fit(approach, dm)
     trainer.save_checkpoint(ckpt_path)
 
