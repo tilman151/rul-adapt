40424d4cb571b4921e1f329ea1def93b24270841
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-08-18 22:07:01 +0200

    test: name of tested function

--- tests/test_tune.py
+++ tests/test_tune.py
@@ -1,12 +1,12 @@
 from rul_adapt import tune
 
 
-def test__flatten_dict():
+def test_config2overrides():
     d = {
         "training": {"approach": {"lr": 0.1, "dann_factor": 2.0}},
         "domain_disc": {"units": [32, 1]},
     }
-    flat = tune._flatten_dict(d)
+    flat = tune.config2overrides(d)
 
     exp_flat = [
         "training.approach.lr=0.1",
