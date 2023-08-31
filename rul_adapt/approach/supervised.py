f5ca137e9f09469aea993adc5f359f27ba0d7237
Tilman Krokotsch <t.krokotsch@gmail.com>
2023-08-31 08:56:23 +0200

    feat: add test step to supervised approach (#47)
    * feat: add test step for adaption to supervised
    
    * feat: make test and val possible with and without adaption data module
    
    * fix: linting issues

--- rul_adapt/approach/supervised.py
+++ rul_adapt/approach/supervised.py
@@ -6,14 +6,14 @@ Data --> FeatureExtractor --> Features --> Regressor  --> RUL Prediction
 ```
 """
 
-from typing import Tuple, Literal, Any, Dict
+from typing import Literal, Any, Dict, List
 
 import torch
 import torchmetrics
 
 from rul_adapt import utils
 from rul_adapt.approach.abstract import AdaptionApproach
-from rul_adapt.approach.evaluation import filter_batch
+from rul_adapt.approach.evaluation import filter_batch, AdaptionEvaluator
 
 
 class SupervisedApproach(AdaptionApproach):
@@ -39,6 +39,7 @@ class SupervisedApproach(AdaptionApproach):
         self,
         loss_type: Literal["mse", "mae", "rmse"],
         rul_scale: int = 1,
+        rul_score_mode: Literal["phm08", "phm12"] = "phm08",
         evaluate_degraded_only: bool = False,
         **optim_kwargs: Any,
     ) -> None:
@@ -52,6 +53,7 @@ class SupervisedApproach(AdaptionApproach):
         see [here][rul_adapt.utils.OptimizerFactory].
 
         Args:
+            rul_score_mode:
             loss_type: Training loss function to use. Either 'mse', 'mae' or 'rmse'.
             rul_scale: Scalar to multiply the RUL prediction with.
             evaluate_degraded_only: Whether to only evaluate the RUL score on degraded
@@ -62,12 +64,17 @@ class SupervisedApproach(AdaptionApproach):
 
         self.loss_type = loss_type
         self.rul_scale = rul_scale
+        self.rul_score_mode = rul_score_mode
         self.evaluate_degraded_only = evaluate_degraded_only
         self.optim_kwargs = optim_kwargs
 
         self.train_loss = utils.get_loss(loss_type)
         self._get_optimizer = utils.OptimizerFactory(**self.optim_kwargs)
         self.val_loss = torchmetrics.MeanSquaredError(squared=False)
+        self.test_loss = torchmetrics.MeanSquaredError(squared=False)
+        self.evaluator = AdaptionEvaluator(
+            self.forward, self.log, self.rul_score_mode, self.evaluate_degraded_only
+        )
 
         self.save_hyperparameters()
 
@@ -77,9 +84,7 @@ class SupervisedApproach(AdaptionApproach):
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         return self.regressor(self.feature_extractor(inputs)) * self.rul_scale
 
-    def training_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
-    ) -> torch.Tensor:
+    def training_step(self, batch: List[torch.Tensor], batch_idx: int) -> torch.Tensor:
         """
         Execute one training step.
 
@@ -101,7 +106,10 @@ class SupervisedApproach(AdaptionApproach):
         return loss
 
     def validation_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+        self,
+        batch: List[torch.Tensor],
+        batch_idx: int,
+        dataloader_idx: int = -1,
     ) -> None:
         """
         Execute one validation step.
@@ -114,7 +122,50 @@ class SupervisedApproach(AdaptionApproach):
             batch: A list of feature and label tensors.
             batch_idx: The index of the current batch.
         """
-        inputs, labels = filter_batch(*batch, degraded_only=self.evaluate_degraded_only)
+        if dataloader_idx == -1:
+            self._no_adapt_validation_step(batch)
+        else:
+            domain = utils.dataloader2domain(dataloader_idx)
+            self.evaluator.validation(batch, domain)
+
+    def _no_adapt_validation_step(self, batch: List[torch.Tensor]) -> None:
+        inputs, labels = batch
+        inputs, labels = filter_batch(
+            inputs, labels, degraded_only=self.evaluate_degraded_only
+        )
         predictions = self.forward(inputs)
         self.val_loss(predictions, labels[:, None])
         self.log("val/loss", self.val_loss)
+
+    def test_step(
+        self, batch: List[torch.Tensor], batch_idx: int, dataloader_idx: int = -1
+    ) -> None:
+        """
+        Execute one test step.
+
+        The `batch` argument is a list of two tensors representing features and
+        labels. A RUL prediction is made from the features and the validation RMSE
+        and RUL score are calculated. The metrics recorded for dataloader idx zero
+        are assumed to be from the source domain and for dataloader idx one from the
+        target domain. The metrics are written to the configured logger under the
+        prefix `test`.
+
+        Args:
+            batch: A list containing a feature and a label tensor.
+            batch_idx: The index of the current batch.
+            dataloader_idx: The index of the current dataloader (0: source, 1: target).
+        """
+        if dataloader_idx == -1:
+            self._no_adapt_test_step(batch)
+        else:
+            domain = utils.dataloader2domain(dataloader_idx)
+            self.evaluator.test(batch, domain)
+
+    def _no_adapt_test_step(self, batch: List[torch.Tensor]) -> None:
+        inputs, labels = batch
+        inputs, labels = filter_batch(
+            inputs, labels, degraded_only=self.evaluate_degraded_only
+        )
+        predictions = self.forward(inputs)
+        self.test_loss(predictions, labels[:, None])
+        self.log("test/loss", self.test_loss)
